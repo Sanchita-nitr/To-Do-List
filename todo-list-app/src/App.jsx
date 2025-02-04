f6bcd57f523479856317228bddfa389d4c812fea
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-02-04 08:31:50 +0530

    Initial commit

---
+++ todo-list-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TodoList from "./components/TodoList.jsx";
+
+function App() {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center" }}>To-Do List</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-list-app/src/components/TodoList.jsx
@@ -0,0 +1,84 @@
+import DeleteIcon from "@mui/icons-material/Delete";
+import {
+  Button,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  TextField,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const TodoList = () => {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTask = () => {
+    if (input.trim() !== "") {
+      setTasks([...tasks, input]);
+      setInput("");
+    }
+  };
+
+  const removeTask = (index) => {
+    setTasks(tasks.filter((_, i) => i !== index));
+  };
+
+  return (
+    <div
+      style={{
+        padding: 20,
+        maxWidth: 400,
+        margin: "auto",
+        backgroundColor: "#00000080",
+        borderStyle: "solid",
+        borderColor: "skyblue",
+        borderRadius: 10,
+      }}
+    >
+      <TextField
+        label="New Task"
+        variant="outlined"
+        fullWidth
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        className="input"
+      />
+      <Button
+        className="task"
+        onClick={addTask}
+        variant="contained"
+        style={{
+          marginTop: 10,
+          width: "100%",
+          backgroundColor: " skyblue",
+          color: "black",
+        }}
+      >
+        Add Task
+      </Button>
+      <List>
+        {tasks.map((task, index) => (
+          <ListItem
+            key={index}
+            secondaryAction={
+              <IconButton
+                style={{ color: "red" }}
+                className="delete"
+                edge="end"
+                aria-label="delete"
+                onClick={() => removeTask(index)}
+              >
+                <DeleteIcon />
+              </IconButton>
+            }
+          >
+            <ListItemText primary={task} />
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+};
+
+export default TodoList;
