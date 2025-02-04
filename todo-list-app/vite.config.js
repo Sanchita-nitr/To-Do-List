5c5029a708b81f706cf3b5fa54e71b41d9ea9d46
Sanchita-nitr <sanchitapriyadarshinee@gmail.com>
2025-02-04 09:04:41 +0530

    final commit

--- todo-list-app/vite.config.js
+++ todo-list-app/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/todo-list-app/',
-  plugins: [react()],
+  base: '/To-Do-List/', // Replace with your repository name
+  plugins: [react()]
 })
